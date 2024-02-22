2d0524f8d19e3d46b7515d0ed6900232cf2698ad
Vinay Chowdry <vinaychowdry31@gmail.com>
2024-02-22 17:08:37 -0500

    Implementing routing for home page and page search page using react-router-dom->BrowserRouter

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,14 +4,24 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
-export const App= () => {
+export const App = () => {
   return (
     <div>
-      <Navbar/>
-      {/* <HomePage/> */}
-      <SearchBooksPage/>
-      <Footer/>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home"/>
+        </Route>
+        <Route path="/home">
+          <HomePage/>
+        </Route>
+        <Route path="/search">
+          <SearchBooksPage />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
   );
 }
